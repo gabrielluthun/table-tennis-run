a909f3811a6385197f612e0247dfceaff92a837f
Gabriel Luthun <gabriel.luthun@gmail.com>
2026-09-08 16:41:01 +0200

    feat: add game rule constants

---
+++ src/constants/gameRules.ts
@@ -0,0 +1,8 @@
+export const MIN_SET_SCORE = 21
+export const MIN_WIN_MARGIN = 2
+export const BO5_WIN_SETS = 3
+export const MAX_SETS = 5
+export const MIN_SETS = 3
+export const BUBBLE_THRESHOLD = 10
+export const DEUCE_TOTAL_THRESHOLD = 40
+export const DEFAULT_VISIBLE_SETS = 3
